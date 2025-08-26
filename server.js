d3853b5dc619436321be837cf9307bbc793eac19
NQGamingVN <quangnormal.dll@gmail.com>
2025-08-26 19:31:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.post('/api/predict', async (req, res) => {
     const prompt = req.body.prompt;
     if (!prompt) return res.status(400).json({ error: "Thiếu prompt" });
 
-    const apiKey = process.env.DEEPSEEK_KEY; // Thay bằng key của bạn
+    const apiKey = "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"; // Thay bằng key của bạn
     const response = await fetch('https://api.openrouter.ai/v1/engines/deepseek/deepseek-v3-0324/completions', {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${apiKey}`,
+        "Authorization": `Bearer ${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}`,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
